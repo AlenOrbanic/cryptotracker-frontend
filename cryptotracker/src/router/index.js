284f0d1dd06c6f4a2edb1d9bb2dba7a5db268bb9
LukaBrodaric <brodaricluka81@gmail.com>
2023-03-04 17:34:46 +0100

    Promjenjena imena ruta

--- cryptotracker/src/router/index.js
+++ cryptotracker/src/router/index.js
@@ -1,22 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/LandingPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'LandingPage',
     component: HomeView
   },
   {
-    path: '/About',
-    name: 'About',
+    path: '/Login',
+    name: 'Login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   }
 ]
 
