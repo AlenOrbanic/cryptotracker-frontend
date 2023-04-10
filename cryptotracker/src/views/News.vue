88c44c1ce1f5d655fcf052d30211185ea87734e9
LukaBrodaric <brodaricluka81@gmail.com>
2023-04-10 11:37:57 +0200

    fix proslog commita

---
+++ cryptotracker/src/views/News.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="portfolio_wrap">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 mx-auto">
+          <div class="lastest_news">
+            <h5>Latest news</h5>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+            <h6>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ cryptotracker/src/views/Settings.vue
@@ -0,0 +1,266 @@
+<script setup>
+import { ref, computed, onMounted, onBeforeUnmount } from "vue";
+const notifications = ref(false);
+const collapseOne = ref(false);
+const collapseTwo = ref(false);
+</script>
+<template>
+  <div class="container">
+    <div class="profile_info mt-md-4 mt-2">
+      <h5>General settings</h5>
+      <div class="accordion" id="ProfileInfo">
+        <div class="accordion-item">
+          <h2 class="accordion-header position-relative" id="notify">
+            <div
+              class="toggle_container"
+              :class="notifications ? 'toggled' : ''"
+              @click="notifications = !notifications"
+            >
+              <div class="toggle"></div>
+            </div>
+            <button
+              class="accordion-button"
+              :class="!collapseOne ? 'collapsed' : ''"
+              type="button"
+              @click="collapseOne = !collapseOne"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="false"
+              aria-controls="collapseOne"
+            >
+              <div class="info_line">
+                <h6>Notifications</h6>
+              </div>
+            </button>
+          </h2>
+          <div
+            id="collapseOne"
+            class="accordion-collapse collapse"
+            :class="collapseOne ? 'show' : ''"
+            aria-labelledby="notify"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <h6>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                aliquam erat id faucibus porta. Orci varius natoque penatibus et
+                magnis dis parturient montes, nascetur ridiculus mus. Etiam
+                fringilla orci et felis rhoncus, eget dignissim purus euismod.
+                Pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas.
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="ChangeCurrency">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              <div class="info_line">
+                <h6>Change currency</h6>
+                <h6>$ - USD</h6>
+              </div>
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            class="accordion-collapse collapse"
+            aria-labelledby="ChangeCurrency"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <div class="changeCurrency_wrap">
+                <div class="changeCurrency_box flex_align">$ - USD</div>
+                <div class="changeCurrency_box flex_align">€- EUR</div>
+                <div class="changeCurrency_box flex_align">¥- JPY</div>
+                <div class="changeCurrency_box flex_align">£- GBP</div>
+                <div class="changeCurrency_box flex_align">₹- INR</div>
+                <div class="changeCurrency_box flex_align">₽- RUB</div>
+                <div class="changeCurrency_box flex_align">₺- TRY</div>
+                <div class="changeCurrency_box flex_align">P - PLN</div>
+                <div class="changeCurrency_box flex_align">N - NOK</div>
+                <div class="changeCurrency_box flex_align">C- CAD</div>
+                <div class="changeCurrency_box flex_align">A- AUD</div>
+                <div class="changeCurrency_box flex_align">H- HUF</div>
+                <div class="changeCurrency_box flex_align">S- SEK</div>
+                <div class="changeCurrency_box flex_align">C- CZK</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="Version">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              <div class="info_line">
+                <h6>Version</h6>
+              </div>
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            class="accordion-collapse collapse"
+            aria-labelledby="Version"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <h6>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                aliquam erat id faucibus porta. Orci varius natoque penatibus et
+                magnis dis parturient montes, nascetur ridiculus mus. Etiam
+                fringilla orci et felis rhoncus, eget dignissim purus euismod.
+                Pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas.
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="TermsService">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFour"
+              aria-expanded="false"
+              aria-controls="collapseFour"
+            >
+              <div class="info_line">
+                <h6>Terms of service</h6>
+              </div>
+            </button>
+          </h2>
+          <div
+            id="collapseFour"
+            class="accordion-collapse collapse"
+            aria-labelledby="TermsService"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <h6>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                aliquam erat id faucibus porta. Orci varius natoque penatibus et
+                magnis dis parturient montes, nascetur ridiculus mus. Etiam
+                fringilla orci et felis rhoncus, eget dignissim purus euismod.
+                Pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas.
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="PrivacyPolicy">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFive"
+              aria-expanded="false"
+              aria-controls="collapseFive"
+            >
+              <div class="info_line">
+                <h6>Privacy policy</h6>
+              </div>
+            </button>
+          </h2>
+          <div
+            id="collapseFive"
+            class="accordion-collapse collapse"
+            aria-labelledby="PrivacyPolicy"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <h6>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                aliquam erat id faucibus porta. Orci varius natoque penatibus et
+                magnis dis parturient montes, nascetur ridiculus mus. Etiam
+                fringilla orci et felis rhoncus, eget dignissim purus euismod.
+                Pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas.
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="Help">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseSix"
+              aria-expanded="false"
+              aria-controls="collapseSix"
+            >
+              <div class="info_line">
+                <h6>Help</h6>
+              </div>
+            </button>
+          </h2>
+          <div
+            id="collapseSix"
+            class="accordion-collapse collapse"
+            aria-labelledby="Help"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <h6>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                aliquam erat id faucibus porta. Orci varius natoque penatibus et
+                magnis dis parturient montes, nascetur ridiculus mus. Etiam
+                fringilla orci et felis rhoncus, eget dignissim purus euismod.
+                Pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas.
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="ContactUS">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseSeven"
+              aria-expanded="false"
+              aria-controls="collapseSeven"
+            >
+              <div class="info_line">
+                <h6>Contact us</h6>
+                <h6 class="clr_blue">info@cryptotracker.com</h6>
+              </div>
+            </button>
+          </h2>
+          <div
+            id="collapseSeven"
+            class="accordion-collapse collapse"
+            aria-labelledby="ContactUS"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="accordion-body">
+              <h6>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                aliquam erat id faucibus porta. Orci varius natoque penatibus et
+                magnis dis parturient montes, nascetur ridiculus mus. Etiam
+                fringilla orci et felis rhoncus, eget dignissim purus euismod.
+                Pellentesque habitant morbi tristique senectus et netus et
+                malesuada fames ac turpis egestas.
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
