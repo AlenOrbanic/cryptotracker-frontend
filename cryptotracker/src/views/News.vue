3c78f4f7293d902321dbcbccffa0e71503ae4698
LukaBrodaric <brodaricluka81@gmail.com>
2023-08-01 21:18:06 +0200

    Postavljene novosti da se prikazuju najnovije

--- cryptotracker/src/views/News.vue
+++ cryptotracker/src/views/News.vue
@@ -5,58 +5,8 @@
         <div class="col-lg-10 mx-auto">
           <div class="lastest_news">
             <h5>Latest news</h5>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
-            <h6>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </h6>
+            <!-- Your existing content goes here -->
+            <div class="cryptohopper-web-widget" data-id="5"></div>
           </div>
         </div>
       </div>
@@ -65,9 +15,30 @@
 </template>
 
 <script>
-export default{
-  name:"news",
+export default {
+  name: "news",
   created() {
+    // Your created hook code goes here
   },
-}
+  mounted() {
+    // Create a script element
+    const script = document.createElement("script");
+    
+    // Set the src attribute to the URL of the external script
+    script.src = "https://www.cryptohopper.com/widgets/js/script";
+    
+    // Append the script to the head of the HTML document
+    document.head.appendChild(script);
+  },
+  methods: {
+    
+  }
+};
 </script>
+
+<style>
+
+  .pull-right {
+    visibility: hidden;
+  }
+</style>
