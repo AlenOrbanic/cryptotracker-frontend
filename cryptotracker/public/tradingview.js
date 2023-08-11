7011adfc80337667cc7db72d37f6ad01cb036674
LukaBrodaric <brodaricluka81@gmail.com>
2023-08-11 20:24:26 +0200

    tradingview js

---
+++ cryptotracker/public/tradingview.js
@@ -0,0 +1,12 @@
+new TradingView.widget({
+  autosize: true,
+  symbol: "COINBASE:BTCUSD",
+  interval: "D",
+  timezone: "Etc/UTC",
+  theme: "light",
+  style: "1",
+  locale: "en",
+  enable_publishing: false,
+  allow_symbol_change: true,
+  container_id: "tradingview_0de26",
+});
