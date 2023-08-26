1ce8a184972944394f2406fc4642ab898fa77d45
AlenOrbanic <aorbanic@student.unipu.hr>
2023-08-26 04:42:14 +0200

    Update vite.config.js

--- cryptotracker/vite.config.js
+++ cryptotracker/vite.config.js
@@ -1,8 +1,6 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
 import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
